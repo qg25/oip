fde31245c219ec4639945ed97d842c1491820ee9
jj-dk <1700010@sit.singaporetech.edu.sg>
2021-08-29 16:17:15 +0800

    Updated GUI

--- src/ReSyringe1/app.py
+++ src/ReSyringe1/app.py
@@ -2,30 +2,48 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging, abort, jsonify, make_response
 import json
 import requests
-import ast
+# import ast
 import cv2
 from os import path
-import os
 import numpy as np
 import base64
-import yolov5
+# import yolov5
+from test import *
 
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/home')
 def index():
     return render_template("index.html")
 
 
-# @app.route('/fullcycle', methods=['GET'])
-# def fullcycle():
+@app.route('/wash_syringes')
+def wash_syringes():
+    return render_template('washSyringe.html')
 
 
-# @app.route('/halfcycle')
-# def halfcycle(): 
+@app.route('/dry_syringes')
+def dry_syringes():
+    return render_template('drySyringe.html')
 
+
+@app.route('/check_stains')
+def check_stains():
+    # Run model to detect for stains and return value gotStains
+    # gotStains 1 or 0
+    gotStains = testFunction1()
+    return jsonify(result=gotStains)
+
+
+@app.route('/check_wetness')
+def check_wetness():
+    # Run model to detect for dry or wet and return value isWet
+    # isWet 1 or 0
+    isWet = testFunction2()
+    return jsonify(result=isWet)
 
 
 if __name__ == '__main__':
-       app.run()
+    app.run(host='0.0.0.0')
